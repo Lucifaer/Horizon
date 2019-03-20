87666bf36a161d8851c5116bb5fe54cb1cf9a948
Lucifaer <l.lucifaer@gmail.com>
2019-03-20 18:49:36 +0800

    Filter用于将数据处理成同一的格式，并测试完毕，现在config文件中的date应利用xpath来捕捉准确的字段

--- core/base/Selector.py
+++ core/base/Selector.py
@@ -1,3 +1,4 @@
+from .Filter import Filter
 from bs4 import BeautifulSoup
 from lxml import etree
 from pprint import pprint
@@ -6,9 +7,9 @@ from pprint import pprint
 class Selector(object):
     def __init__(self, config, content):
         self.rules = config['spider']['rules']
-        self.url = config['spider']['start_url']
         self.selector = etree.HTML(content)
         self.content = content
+        self.filter = Filter(config['spider']['start_url'])
 
     def get_data(self):
         items = dict()
@@ -22,47 +23,25 @@ class Selector(object):
         for i in range(len(items['title_link'])):
             try:
                 info_collections = {
-                    'title': self.translate_word(items['title_link'][i].text),
-                    'link': self.create_full_links(items['title_link'][i].attrib.get('href')),
+                    'title': self.filter.handle_title(items['title_link'][i].text),
+                    'link': self.filter.handle_url(items['title_link'][i].attrib.get('href')),
                     'tag': items['tag'][i].text,
-                    'date': items['date'][i],
+                    'date': self.filter.handle_date(items['date'][i]),
                 }
             except IndexError as tag_err:
                 info_collections = {
-                    'title': self.translate_word(items['title_link'][i].text),
-                    'link': self.create_full_links(items['title_link'][i].attrib.get('href')),
+                    'title': self.filter.handle_title(items['title_link'][i].text),
+                    'link': self.filter.handle_url(items['title_link'][i].attrib.get('href')),
                     'tag': '未知',
-                    'date': items['date'][i].text,
+                    'date': self.filter.handle_date(items['date'][i].text),
                 }
             item.append(info_collections)
-        pprint(item)
         try:
-            next_page = self.create_full_links(items['next_page'][0])
+            next_page = self.filter.handle_url(items['next_page'][0].attrib.get('href'))
+            pprint(next_page)
         except IndexError as next_page_err:
-            pprint(next_page_err)
             next_page = None
         return item, next_page
-
-    @staticmethod
-    def translate_word(word):
-        table = {ord(f): ord(t) for f, t in zip(
-            u'，。！？：【】（）/％＃＠＆１２３４５６７８９０',
-            u',.!?:[]()-%#@&1234567890'
-        )}
-        return word.translate(table)
-
-    def create_full_links(self, short_tag):
-        full_links = ''
-        link_dict = (self.url + short_tag).split('/')
-        for i in range(len(link_dict)):
-            if link_dict[i] != '':
-                full_links += link_dict[i]
-            else:
-                if i == 1:
-                    full_links += '//'
-                else:
-                    full_links += '/'
-        return full_links
 
     def test(self):
         items = dict()
